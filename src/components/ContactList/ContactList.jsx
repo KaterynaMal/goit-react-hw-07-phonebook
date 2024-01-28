2a6b484906b61ab55a4dc91975bf56ae17d04c60
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-01-28 17:20:43 +0200

    return Array.isArray

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,22 +5,24 @@ import { removeContact } from '../../redux/Contacts/contactsReducer';
 
 const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(store => store.contacts.contacts.items);
+  const contacts = useSelector(store => store.contacts.items);
   const filter = useSelector(store => store.contacts.filter);
 
   const handleDeleteContact = id => {
     dispatch(removeContact(id));
   };
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredContacts = Array.isArray(contacts)
+    ? contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : [];
 
   return (
     <div>
       <ul>
-        {filteredContacts.length > 0 ? (
-          filteredContacts.map(contact => (
+        {Array.isArray(filter ? filteredContacts : contacts) &&
+          (filter ? filteredContacts : contacts).map(contact => (
             <li className={css.contact_item} key={contact.id}>
               {contact.name}: {contact.number}
               <button
@@ -31,13 +33,67 @@ const ContactList = () => {
                 Delete
               </button>
             </li>
-          ))
-        ) : (
-          <li key="no-matching-contacts">No matching contacts found</li>
-        )}
+          ))}
+        {Array.isArray(filter ? filteredContacts : contacts) &&
+          (filter ? filteredContacts : contacts).length === 0 && (
+            <li>No matching contacts found</li>
+          )}
       </ul>
     </div>
   );
 };
 
 export { ContactList };
+
+
+
+
+// import React from 'react';
+// import css from './ContactList.module.css';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { removeContact } from '../../redux/Contacts/contactsReducer';
+
+// const ContactList = () => {
+//   const dispatch = useDispatch();
+//   const contacts = useSelector(store => store.contacts.contacts.items);
+//   const filter = useSelector(store => store.contacts.filter);
+
+//   const handleDeleteContact = id => {
+//     dispatch(removeContact(id));
+//   };
+
+//     const filteredContacts = Array.isArray(contacts)
+//     ? contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase())
+//     )
+//     : [];
+
+//   return (
+//     <div>
+//       <ul>
+//          {Array.isArray(filter ? filteredContacts : contacts) && 
+//           (filter ? filteredContacts : contacts).map(contact => (
+//             {
+//               filteredContacts.length > 0 ? (
+//                 filteredContacts.map(contact => (
+//                   <li className={css.contact_item} key={contact.id}>
+//                     {contact.name}: {contact.number}
+//                     <button
+//                       type="button"
+//                       className={css.delete_btn}
+//                       onClick={() => handleDeleteContact(contact.id)}
+//                     >
+//                       Delete
+//                     </button>
+//                   </li>
+//                 ))}
+//         {Array.isArray(filter ? filteredContacts : contacts) &&
+//           (filter ? filteredContacts : contacts).length === 0 && (
+//           <li key="no-matching-contacts">No matching contacts found</li>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export { ContactList };
