59887b8bf7b78ac2b598402664c05a7d12c20965
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-01-31 19:55:23 +0200

    +

--- src/redux/Contacts/contactsReducer.js
+++ src/redux/Contacts/contactsReducer.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, createAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import { requestContacts, createContact, deleteContactApi } from 'services/api';
 
 export const apiGetContacts = createAsyncThunk(
